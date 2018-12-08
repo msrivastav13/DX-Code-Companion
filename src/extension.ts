ec4dfc75d81c253d162ec321894479a3abe4cc1b
Mohith Shrivastava <mohith@codescience.com>
2018-12-08 17:12:23 -0500

    change uri path to use from callback

--- src/extension.ts
+++ src/extension.ts
@@ -8,24 +8,17 @@ import {CommandService} from './services/commandBuilder';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    // console.log('Congratulations, your extension "dx-code-companion" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let retrieveSource = vscode.commands.registerCommand('retrieve.dxSource', () => {
         // The code you place here will be executed every time your command is executed
         retrieveDXSource();
     });
 
-    let deploySource = vscode.commands.registerCommand('deploy.source', () => {
+    let deploySource = vscode.commands.registerCommand('deploy.source', (uri:vscode.Uri) => {
         let activeTerminal = setupTerminal();
         if(activeTerminal){
             if(vscode.window.activeTextEditor){
-                const path = vscode.window.activeTextEditor.document.uri.fsPath;
-                const commandToExecute = new CommandService(path);
+                // const path = vscode.window.activeTextEditor.document.uri.fsPath;
+                const commandToExecute = new CommandService(uri.fsPath);
                 activeTerminal.sendText(commandToExecute.generateCommand());
             }
         }
